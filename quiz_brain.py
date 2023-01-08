7b86cc216135515dd69ea7d9b6a5f938f5fd91d1
VachhaniNirav <99639408+VachhaniNirav@users.noreply.github.com>
2023-01-08 23:32:39 +0530

    python basics: GUI_tkinter

---
+++ quiz_brain.py
@@ -0,0 +1,30 @@
+import html
+
+
+class QuizBrain:
+
+    def __init__(self, questions_list):
+        self.question_number = 0
+        self.questions_list = questions_list
+        self.score = 0
+
+    def next_question(self):
+        """Returns the next question as STR."""
+        self.question_number += 1
+        # unescape characters in the question text if necessary
+        fixed_text = html.unescape(self.questions_list[self.question_number - 1].text)
+        # the displayed question number doesn't need the + 1 anymore
+        return f"Q.{self.question_number}: {fixed_text}"
+
+    def still_has_questions(self):
+        """Checks if there are questions remaining and returns a BOOL."""
+        if self.question_number < len(self.questions_list):
+            return True
+        return False
+
+    def check_answer(self, choice):
+        """Takes a STR, compares it to the current answer and returns a BOOL."""
+        if choice == self.questions_list[self.question_number - 1].answer:
+            self.score += 1
+            return True
+        return False
